987d2b1d7df4b039da6e9f3722ce7559c4e037f0
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-09-15 19:19:31 -0400

    style landing page

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ const Oils = require('./models/oils')
 //DATABASE CONFIGURATION
 mongoose.connect(process.env.DATABASE_URL)
 
-
-
-
 const db = mongoose.connection
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
